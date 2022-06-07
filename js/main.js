efc453b88be670314aa8917374629a2b656f33bc
pagemaker2022 <98583359+pagemaker2022@users.noreply.github.com>
2022-06-07 21:23:14 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(function () {
+  $(".menu a, .footer__arrow-up").on("click", function (e) {
+    //отменяем стандартную обработку нажатия по ссылке
+    e.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr('href'),
+
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+  $('.slider-blog__inner').slick({
+    dots: true,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
+    responsive: [
+      {
+        breakpoint: 701,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  $('.menu__btn, .menu a').on('click', function () {
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+
+  var mixer = mixitup('.portfolio__content', {
+    animation: {
+      duration: 1200
+    }
+  });
+
+});
